c590d7ff350fe1685ccb332482719d065b66572e
Mateo Castillo <mate.dev9618@gmail.com>
2024-03-09 16:10:22 -0500

    jwt verrify

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,22 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { verifyAuth } from "./app/(routes)/hub/lib/auth";
 
 export const config = {
   matcher: ["/hub/:path*"],
 };
 
-export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get("currentUser")?.value;
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("currentUser")?.value;
 
-  if (currentUser && !request.nextUrl.pathname.startsWith("/hub/my-profile")) {
+  const tokenVerify =
+    token && (await verifyAuth(token).catch((err) => console.log(err)));
+
+  if (tokenVerify && !request.nextUrl.pathname.startsWith("/hub/my-profile")) {
     return NextResponse.redirect(new URL("/hub/my-profile", request.url));
   }
 
-  if (!currentUser && !request.nextUrl.pathname.startsWith("/hub/login")) {
+  if (!tokenVerify && !request.nextUrl.pathname.startsWith("/hub/login")) {
     if (request.nextUrl.pathname.startsWith("/hub/registration")) {
       return NextResponse.next();
     }
